f841c0494bc34cb532f679543c9242ef680d29ef
Christos Konstantas <ckonstantas@gmail.com>
2020-11-04 22:48:05 +0200

    End of Section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,16 +2,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using API.Data;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using API.Services;
-using API.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
@@ -40,14 +33,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // If we are in development mode and the application encounters a problem
-            // then a developer extception page will be triggered
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             // if we come from http we will be redirected to https
             app.UseHttpsRedirection();
